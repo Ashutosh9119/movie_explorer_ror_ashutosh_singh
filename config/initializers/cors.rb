68824cd28e537379c5703783b944e7959675e402
Ashutosh <asmednipur@gmail.com>
2025-05-01 12:22:30 +0530

    [Ashutosh] Mod: Handled Cors for frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins [
+      'http://localhost:3000', # Local frontend development
+      'https://movie-explorer-frontend.onrender.com' # Replace with your actual frontend production URL
+    ]
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
