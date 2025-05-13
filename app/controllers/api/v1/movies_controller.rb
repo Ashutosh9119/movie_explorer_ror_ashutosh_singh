68cf95bdd197d7e164c0f680533b122347c1eeea
Ashutosh <asmednipur@gmail.com>
2025-05-13 15:56:00 +0530

    [Ashutosh] Mod: DOne some changes im Movies Controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,23 +2,11 @@ module Api
   module V1
     class MoviesController < ApplicationController
       skip_before_action :verify_authenticity_token
-      before_action :authenticate_user!, only: [:create, :update, :destroy]
+      before_action :authenticate_user!
       before_action :authorize_action!, only: [:create, :update, :destroy]
 
       def index
         movies = Movie.api_search_filter_paginate(index_params)
-
-        # If the user is authenticated, check their subscription status
-        if current_user
-          subscription = current_user.subscription
-          unless subscription&.active?
-            movies = movies.where(is_premium: false)
-          end
-        else
-          # Unauthenticated users can only see non-premium movies
-          movies = movies.where(is_premium: false)
-        end
-
         render json: {
           movies: movies.as_json(methods: [:banner_url, :poster_url]),
           total_pages: movies.total_pages,
@@ -30,69 +18,43 @@ module Api
 
       def show
         movie = Movie.find(params[:id])
-
-        # If the movie is premium, enforce subscription check
         if movie.is_premium
-          unless current_user
-            render json: { error: "Unauthorized: Please log in to access this movie" }, status: :unauthorized
-            return
-          end
-
           subscription = current_user.subscription
-          unless subscription&.active?
-            render json: { error: "Please purchase an active subscription to access this movie" }, status: :forbidden
-            return
-          end
+          render json: { error: "Please purchase a subscription to access this premium movie" }, status: :forbidden unless subscription&.status == "active"
+          render json: { error: "Please purchase a premium subscription to access this movie" }, status: :forbidden unless has_active_premium_subscription?(subscription)
         end
-
         render json: movie.as_json(methods: [:banner_url, :poster_url]), status: :ok
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: "Movie not found" }, status: :not_found
       end
 
       def create
         movie = Movie.new(movie_params)
-        if movie.save
-          send_new_movie_notification(movie)
-          if params[:movie][:banner].present? && !movie.banner.attached?
-            movie.banner.attach(params[:movie][:banner])
-          end
-          if params[:movie][:poster].present? && !movie.poster.attached?
-            movie.poster.attach(params[:movie][:poster])
-          end
-          render json: movie.as_json(methods: [:banner_url, :poster_url]), status: :created
-        else
-          render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
+        movie.save
+        send_new_movie_notification(movie)
+        if params[:movie][:banner].present? && !movie.banner.attached?
+          movie.banner.attach(params[:movie][:banner])
         end
-      rescue StandardError => e
-        render json: { errors: ["Failed to process images: #{e.message}"] }, status: :unprocessable_entity
+        if params[:movie][:poster].present? && !movie.poster.attached?
+          movie.poster.attach(params[:movie][:poster])
+        end
+        render json: movie.as_json(methods: [:banner_url, :poster_url]), status: :created
       end
 
       def update
         movie = Movie.find(params[:id])
-        if movie.update(movie_params)
-          if params[:movie][:banner].present? && (!movie.banner.attached? || params[:movie][:banner].original_filename != movie.banner.filename)
-            movie.banner.attach(params[:movie][:banner])
-          end
-          if params[:movie][:poster].present? && (!movie.poster.attached? || params[:movie][:poster].original_filename != movie.poster.filename)
-            movie.poster.attach(params[:movie][:poster])
-          end
-          render json: movie.as_json(methods: [:banner_url, :poster_url]), status: :ok
-        else
-          render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
+        movie.update(movie_params)
+        if params[:movie][:banner].present? && (!movie.banner.attached? || params[:movie][:banner].original_filename != movie.banner.filename)
+          movie.banner.attach(params[:movie][:banner])
         end
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: "Movie not found" }, status: :not_found
-      rescue StandardError => e
-        render json: { errors: ["Failed to process images: #{e.message}"] }, status: :unprocessable_entity
+        if params[:movie][:poster].present? && (!movie.poster.attached? || params[:movie][:poster].original_filename != movie.poster.filename)
+          movie.poster.attach(params[:movie][:poster])
+        end
+        render json: movie.as_json(methods: [:banner_url, :poster_url]), status: :ok
       end
 
       def destroy
         movie = Movie.find(params[:id])
         movie.destroy
         head :no_content
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: "Movie not found" }, status: :not_found
       end
 
       private
@@ -111,9 +73,7 @@ module Api
 
       def authorize_action!
         action = action_name.to_sym
-        unless current_user && authorized_for_action?(action)
-          render json: { error: "Unauthorized: Only supervisors can #{action} movies" }, status: :forbidden
-        end
+        render json: { error: "Unauthorized: Only supervisors can #{action} movies" }, status: :forbidden unless current_user && authorized_for_action?(action)
       end
 
       def authorized_for_action?(action)
@@ -125,22 +85,18 @@ module Api
         end
       end
 
+      def has_active_premium_subscription?(subscription)
+        return false unless subscription.plan_type == "premium"
+        return true if subscription.expires_at.nil?
+        subscription.expires_at > Time.current
+      end
+
       def send_new_movie_notification(movie)
         users = User.where(notifications_enabled: true).where.not(device_token: nil)
         return if users.empty?
         device_tokens = users.pluck(:device_token)
-        begin
-          fcm_service = FcmService.new
-          response = fcm_service.send_notification(device_tokens, "New Movie Added!", "#{movie.title} has been added to the Movie Explorer collection.", { movie_id: movie.id.to_s })
-          Rails.logger.info("FCM Response: #{response}")
-          if response[:status_code] == 200
-            Rails.logger.info("FCM Response: #{response}")
-          else
-            Rails.logger.error("FCM Error: #{response[:body]}")
-          end
-        rescue StandardError => e
-          Rails.logger.error("FCM Notification Failed: #{e.message}")
-        end
+        fcm_service = FcmService.new
+        fcm_service.send_notification(device_tokens, "New Movie Added!", "#{movie.title} has been added to the Movie Explorer collection.", { movie_id: movie.id.to_s })
       end
     end
   end
